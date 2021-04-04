c25f317758e1835de29852ced314c60101308d39
wzf <80504767@qq.com>
2021-04-04 22:12:12 +0800

    1.添加热部署,成功访问controller

--- WYVideo/system/src/main/java/pxxy/wzf/system/SystemApplication.java
+++ WYVideo/system/src/main/java/pxxy/wzf/system/SystemApplication.java
@@ -1,4 +1,4 @@
-package pxxy.wzf;
+package pxxy.wzf.system;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
---
+++ WYVideo/system/src/main/java/pxxy/wzf/system/controller/TestController.java
@@ -0,0 +1,13 @@
+package pxxy.wzf.system.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public String test(){
+        return "测试接口是否有用";
+    }
+}
