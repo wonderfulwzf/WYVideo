c1e4b5fa471090ab6e2d4b5cc7b872e17973bf1e
wzf <80504767@qq.com>
2021-04-04 21:56:27 +0800

    1.解决启动报错，配置配置文件

--- WYVideo/eureka/src/main/java/pxxy/wzf/EurekaApplication.java
+++ WYVideo/eureka/src/main/java/pxxy/wzf/EurekaApplication.java
@@ -1,12 +1,25 @@
 package pxxy.wzf;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
+	/**
+	 * 获取日志对象
+	 */
+	private static final Logger LOG = LoggerFactory.getLogger(EurekaApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
+		SpringApplication app = new SpringApplication(EurekaApplication.class);
+		Environment environment = app.run(args).getEnvironment();
+		LOG.info("启动成功！！");
+		LOG.info("Eureka地址:\thttp:127.0.0.1:{}",environment.getProperty("server.port"));
 	}
 
 }
