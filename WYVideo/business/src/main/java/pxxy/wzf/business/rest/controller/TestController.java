26b4ad88fc7b7fa6907131f2f9e305f61431fd8b
wzf <80504767@qq.com>
2021-04-21 17:50:20 +0800

    1.优化TestController的swagger文档

--- WYVideo/business/src/main/java/pxxy/wzf/business/rest/controller/TestController.java
+++ WYVideo/business/src/main/java/pxxy/wzf/business/rest/controller/TestController.java
@@ -3,22 +3,35 @@ package pxxy.wzf.business.rest.controller;
 import io.swagger.annotations.Api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pxxy.wzf.server.service.TestService;
 
 @RestController
 @Api(tags = "接口")
 public class TestController {
+
+    @Autowired
+    private TestService testService;
 
     /**
      * 日志BUSINESS_NAME
      */
     private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
 
-    public static final String BUSINESS_NAME = "";
+    /**
+     * 用户测试
+     */
+    public static final String BUSINESS_NAME = "测试";
 
+    /**
+     * @auther: 王智芳
+     * @Description 用于测试ssm框架连通性
+     * @date: 2021/4/21 17:47
+     */
     @RequestMapping("/test")
     public String test(){
-        return "用于测试";
+        return testService.list().toString();
     }
 }
